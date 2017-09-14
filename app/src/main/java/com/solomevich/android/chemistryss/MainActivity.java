a3e772f657f994f652e52af4eae957f7bdbf1a69
Sergey Solomevich <sergeysolomevich@gmail.com>
2017-09-14 21:57:06 +0300

    commit 0 - созданы классы Equalize и Result, xml - файлы activity_equalize и activity_result для работы уравнивателя реакций.

--- app/src/main/java/com/solomevich/android/chemistryss/MainActivity.java
+++ app/src/main/java/com/solomevich/android/chemistryss/MainActivity.java
@@ -1,7 +1,9 @@
 package com.solomevich.android.chemistryss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(MainActivity.this, Equalize.class);
+        startActivity(intent);
+    }
+
+    public void submitMM(View view) {
+        Intent intent = new Intent(MainActivity.this, Equalize.class);
+        startActivity(intent);
+    }
+
 }
