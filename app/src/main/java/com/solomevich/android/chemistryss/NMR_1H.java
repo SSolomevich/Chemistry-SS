f2560ccb351bf8347200ae0f7361792aa75224e4
Sergey Solomevich <sergeysolomevich@gmail.com>
2017-10-03 00:00:18 +0300

    commit 9

---
+++ app/src/main/java/com/solomevich/android/chemistryss/NMR_1H.java
@@ -0,0 +1,18 @@
+package com.solomevich.android.chemistryss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 15 on 30.09.2017.
+ */
+
+public class NMR_1H extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_nmr_h);
+
+    }
+}
