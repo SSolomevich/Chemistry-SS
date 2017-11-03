1133e80496ab0cd197488cec0b943232470b8142
Sergey Solomevich <sergeysolomevich@gmail.com>
2017-11-03 21:33:45 +0300

    commit 15 - added classes and layouts for reactions

---
+++ app/src/main/java/com/solomevich/android/chemistryss/ReactionsCations.java
@@ -0,0 +1,18 @@
+package com.solomevich.android.chemistryss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 15 on 17.10.2017.
+ */
+
+public class ReactionsCations extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_reactions_cations);
+
+    }
+}
